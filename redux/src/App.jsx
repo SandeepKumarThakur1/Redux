333bd9ede3f533eb8cbbf6213835d1a14b94079d
sandeep Kumar <sandeepkumarthakur9888@gmail.com>
2025-01-22 23:02:25 +0530

    impliment Tailwind Css and Redux toolkit

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="bg-gray-900 p-5 text-white font-sans text-xl">
-        <h1>Redex | Let's Start</h1>
+        <h1>Redex | Let&#39;s Start</h1>
       </div>
       <div className="h-screen flex items-center justify-center text-3xl">
         <CounterApp />
